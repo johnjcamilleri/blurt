4e2557984d76049c6c988664033e61e0d0af1658
John J. Camilleri <john.j.camilleri@chalmers.se>
2025-03-28 15:27:04 +0100

    Add Alpine, but it's not working...

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,32 +1,25 @@
 import { io } from 'socket.io-client';
 import { debounce } from './common';
 
+import Alpine from 'alpinejs';
+
+// Initialize Alpine.js
+// window['Alpine'] = Alpine;
+
+const state = {
+    response: '',
+};
+Alpine.data('state', () => state);
+Alpine.start();
+
 const socket = io();
 
-const responseInput = document.getElementById('responseInput') as HTMLInputElement;
-const clearInputButton = document.getElementById('clearInput') as HTMLButtonElement;
-
-const sendResponse = debounce((response) => {
+const sendResponse = debounce((response: string) => {
     socket.emit('respond', response);
 }, 200);
 
 socket.on('update responses', (responses) => {
     if (socket.id && !responses[socket.id])
-        responseInput.value = '';
+        state.response = '';
 });
 
-responseInput.addEventListener('input', () => {
-    sendResponse(responseInput.value);
-});
-
-document.querySelectorAll('button[data-prefill]').forEach(button => {
-    button.addEventListener('click', () => {
-        responseInput.value = button.textContent || "";
-        sendResponse(button.textContent);
-    });
-});
-
-clearInputButton.addEventListener('click', () => {
-    responseInput.value = '';
-    sendResponse('');
-});
