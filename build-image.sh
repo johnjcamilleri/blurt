01d1992326ac787d28b55108b1677fdc403b3241
John J. Camilleri <john.j.camilleri@chalmers.se>
2025-10-25 19:33:49 +0200

    Remove build and serve scripts

--- build-image.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-# Build Docker image
-docker build . --tag blurt:latest
--- serve-image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Serve Docker image
-docker run \
-  --name "blurt" \
-  --rm \
-  --detach \
-  --publish 3000:3000 \
-  blurt:latest
