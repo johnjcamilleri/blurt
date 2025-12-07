addddd19f7a86e497e8e55e01789d11c120d3806
John J. Camilleri <john.j.camilleri@chalmers.se>
2025-12-07 14:19:00 +0100

    Create room with cmd+enter
    Addresses #28

--- client/src/index.ts
+++ client/src/index.ts
@@ -4,3 +4,24 @@ const versionElem = document.querySelector('#version');
 if (versionElem && packageJson.version) {
     versionElem.textContent = `v${packageJson.version}`;
 }
+
+const roomInput: HTMLInputElement = document.querySelector('#room')!;
+roomInput.addEventListener('keydown', (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+        if (event.metaKey || event.ctrlKey) createButton.click();
+        else joinButton.click();
+    }
+});
+
+const joinButton: HTMLButtonElement = document.querySelector('#join')!;
+joinButton.addEventListener('click', () => {
+    if (roomInput.value) {
+        globalThis.location.href = '/' + roomInput.value;
+    }
+});
+
+const createButton: HTMLButtonElement = document.querySelector('#create')!;
+createButton.addEventListener('click', () => {
+    globalThis.location.href = '/create/' + roomInput.value;
+});
+
