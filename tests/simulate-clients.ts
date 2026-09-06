daadd00b7aefd5a87c18eae60db19397d3e7f19b
John J. Camilleri <john.j.camilleri@chalmers.se>
2026-09-06 10:27:57 +0200

    Add proper arg parsing to simulate script

--- tests/simulate-clients.ts
+++ tests/simulate-clients.ts
@@ -1,21 +1,23 @@
 /* eslint-disable array-element-newline */
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import axios from 'axios';
 import {io, type Socket} from 'socket.io-client';
 
-// Usage: npm run sim [MODE] [NUM_CLIENTS] [NUM_RESPONSES]
+// Usage: npm run sim ...
+const argv = yargs(hideBin(process.argv)).options({
+    url: { type: 'string', default: 'http://localhost:3000'},
+    room: { type: 'string', default: 'testroom'},
+    mode: { choices: ['text', 'number', 'yes-no-maybe'], default: 'text' },
+    clients: { type: 'number', default: 20, describe: 'number of clients' },
+    responses: { type: 'number', default: 10, describe: 'number of possible responses' },
+    active: { type: 'number', default: 1.0, describe: 'fraction of clients that are active (0.0-1.0)' },
+    skew: { type: 'number', default: 2, describe: 'make certain responses more frequent (1 = uniform, >1 favours earlier responses)' },
+}).parseSync()
 
-const SERVER_URL = 'http://localhost:3000';
-const ROOM_NAME = 'testroom';
-const MODE = process.argv[2] || 'text';
-const NUM_CLIENTS = Number(process.argv[3]) || 20;
-const NUM_RESPONSES = Number(process.argv[4]) || 10;
-const INTERVAL_MS = 3000; // delay between loop iterations
-const SEND_DELAY_MS = 2000; // delay sending by up to
-const NUM_MIN = -10;
-const NUM_MAX = 64;
-const ACTIVE_CLIENTS = 1.0; // percentage of clients which are active
-let RESPONSES: string[] = [];
-if (MODE === 'text') {
+const SEND_DELAY_MS = 2000; // spread out responses over this amount of time
+let RESPONSES: string[] = []; // possible responses which can be sent
+if (argv.mode === 'text') {
     const FRUITS = [
         'apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon',
         'mango', 'nectarine', 'orange', 'papaya', 'quince', 'raspberry', 'strawberry', 'tangerine', 'ugli', 'vanilla',
@@ -28,10 +30,10 @@ if (MODE === 'text') {
         'calamondin', 'durian', 'emblic', 'farkleberry', 'gac', 'hornedmelon', 'ilama', 'jostaberry', 'kabosu', 'lucuma',
         'mammee', 'noni', 'osageorange', 'pitanga', 'quenepa', 'ribes', 'santol', 'tamarillo', 'uchuva', 'voavanga',
     ]; // size 100
-    if (NUM_RESPONSES > FRUITS.length) {
+    if (argv.responses > FRUITS.length) {
         // 2-word answers, works up to 100² = 100 000
         const response_set = new Set<string>();
-        while (response_set.size < NUM_RESPONSES) {
+        while (response_set.size < argv.responses) {
             const f1 = FRUITS[Math.floor(Math.random() * FRUITS.length)];
             const f2 = FRUITS[Math.floor(Math.random() * FRUITS.length)];
             response_set.add(`${f1} ${f2}`);
@@ -40,26 +42,28 @@ if (MODE === 'text') {
         RESPONSES = Array.from(response_set);
     } else {
         // 1-word answers, works up to 100
-        RESPONSES = FRUITS.sort(() => 0.5 - Math.random()).slice(0, NUM_RESPONSES);
+        RESPONSES = FRUITS.sort(() => 0.5 - Math.random()).slice(0, argv.responses);
     }
-} else if (MODE === 'number') {
-    RESPONSES = Array.from({length: NUM_RESPONSES}, () =>
+} else if (argv.mode === 'number') {
+    const NUM_MIN = -10;
+    const NUM_MAX = 64;
+    RESPONSES = Array.from({length: argv.responses}, () =>
         Math.floor(Math.random() * (NUM_MAX - NUM_MIN + 1)) + NUM_MIN,
     ).map(String);
-} else if (MODE === 'yes-no-maybe') {
+} else if (argv.mode === 'yes-no-maybe') {
     RESPONSES = ['yes', 'no', 'maybe'];
 }
 
 // Create room
 async function createTestRoom() {
-    await axios.get(`${SERVER_URL}/${ROOM_NAME}`);
+    await axios.get(`${argv.url}/${argv.room}`);
 }
 await createTestRoom();
 
 // Create clients
 const clients: Socket[] = [];
-for (let i = 0; i < NUM_CLIENTS; i++) {
-    const socket = io(SERVER_URL, {query: {roomName: ROOM_NAME}});
+for (let i = 0; i < argv.clients; i++) {
+    const socket = io(argv.url, {query: {roomName: argv.room}});
 
     socket.on('connect', () => {
         console.log(`${socket.id} connected`);
@@ -73,20 +77,18 @@ for (let i = 0; i < NUM_CLIENTS; i++) {
 }
 
 // Send responses
-// setInterval(() => {
-    for (const socket of clients) {
-        setTimeout(() => {
-            if (Math.random() < ACTIVE_CLIENTS) {
-                // Weighted selection: earlier items are more likely
-                const idx = Math.floor((Math.random() ** 2.5) * RESPONSES.length);
-                const response = RESPONSES[idx];
-                socket.emit('respond', response);
-                console.log(`${socket.id} sent: ${response}`);
-            } else {
-                // Send nothing
-                socket.emit('respond', '');
-            }
-        }, Math.floor(Math.random() * SEND_DELAY_MS));
-    }
-// }, INTERVAL_MS);
+for (const socket of clients) {
+    setTimeout(() => {
+        if (Math.random() < argv.active) {
+            // Weighted selection: earlier items are more likely
+            const idx = Math.floor((Math.random() ** argv.skew) * RESPONSES.length);
+            const response = RESPONSES[idx];
+            socket.emit('respond', response);
+            console.log(`${socket.id} sent: ${response}`);
+        } else {
+            // Send nothing
+            socket.emit('respond', '');
+        }
+    }, Math.floor(Math.random() * SEND_DELAY_MS));
+}
 
